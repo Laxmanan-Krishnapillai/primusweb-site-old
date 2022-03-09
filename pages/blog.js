b72e841ba919b9992b48993041b41b9323c83023
unknown <laxmanan.bales@gmail.com>
2022-03-09 11:06:20 +0100

    pushing

---
+++ pages/blog.js
@@ -0,0 +1,36 @@
+import { fetchData } from '../lib/api'
+import { content } from '../tailwind.config'
+import Link from 'next/link'
+import { getStrapiMedia } from '../lib/media'
+import Image from 'next/image';
+import Navbar from './components/navbar';
+export default function Blog({blogposts}) {
+  return(
+    <div className='flex p-8 bg-gradient-to-r from-gray-700 via-gray-900 mt-[10vh] overflow to-black min-h-screen'>
+    <Navbar/>
+    {blogposts.data.map((post)=>{
+      var { title, slug } = post.attributes;
+      var { alternativeText } = post.attributes.featuredImg.data.attributes;
+      var { thumbnail } = post.attributes.featuredImg.data.attributes.formats;
+      return (
+        <Link className="cursor-pointer" href={'/posts/' + slug} passHref key={slug}>
+          <div className='cursor-pointer w-[400px] h-[300px] bg-neutral-900 rounded-2xl overflow-hidden flex flex-col'>
+            <Image className='w-full h-auto' src={getStrapiMedia(thumbnail.url)} width={thumbnail.width} height={thumbnail.height} objectFit="contain" alt={alternativeText}></Image>
+            <h1 className='font-black text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 via-green-200 to-green-300 text-4xl'>{title}</h1>
+          </div>
+        </Link>  
+      )
+    })}
+  </div>
+  )
+}
+
+
+export async function getStaticProps() {
+    const blogposts = await fetchData("/api/blogposts?fields=description,slug,title&populate=%2A");
+    return {
+      props: {
+        blogposts
+      }
+    }
+}
---
+++ pages/posts/[slug].js
@@ -0,0 +1,42 @@
+import React from "react"
+import Markdown from "markdown-to-jsx"
+import { fetchData } from "../../lib/api"
+import Image from 'next/image';
+import { getStrapiMedia } from "../../lib/media";
+export default function Post({ post }) {
+  const { large } = post.featuredImg.data.attributes.formats;
+    return (
+    <div className="w-screen h-screen flex justify-center bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+      <div className="max-w-[500px] mx-auto text-white">  
+        <h1 className="text-6xl font-black">{post.title}</h1>
+        <h2 className="text-2xl font-thin">{post.description}</h2>
+        <Image src={getStrapiMedia(large.url)} alt="featured image" height={large.height} width={large.width} objectFit='contain'></Image>
+        <Markdown>{post.text}</Markdown>
+      </div>
+    </div>
+    )
+  }
+
+  export async function getStaticPaths() {
+    const pathsraw = await fetchData('/api/blogposts?fields=slug');
+    const paths = pathsraw.data.map(item => {
+      return {
+        params: { slug: item.attributes.slug}
+      }
+    });
+    return {
+      paths: paths,
+      fallback: false,
+    }
+  }
+
+export const getStaticProps = async (context) => {
+  const slug = context.params.slug;
+  const blogdataraw = await fetchData(`/api/blogposts?filters[slug]=${slug}&populate=%2A`);
+  const blogdata = blogdataraw.data[0].attributes;
+  return {
+    props: {
+      post: blogdata
+    }
+  }
+}
