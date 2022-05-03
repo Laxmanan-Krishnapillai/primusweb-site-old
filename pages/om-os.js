a3dfd58ff78ab75317dd57a98716f3f4c7aebdf6
unknown <laxmanan.bales@gmail.com>
2022-05-03 19:57:20 +0200

    pushing

--- pages/om-os.js
+++ pages/om-os.js
@@ -10,14 +10,7 @@ export default function OmOs() {
       initial={{ opacity: 0 }}
     >
       <section className="h-screen w-screen flex justify-center flex-col items-center">
-        <div className="">
-          <Image
-            src="/images/om-os.jpg"
-            alt="Billeder af PrimusWeb teamet"
-            width={1200}
-            height={1080}
-          />
-        </div>
+        <div className=""></div>
         <h1 className="text-5xl lg:text-8xl font-black uppercase text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 px-10 mb-2">
           Om os
         </h1>
@@ -25,7 +18,7 @@ export default function OmOs() {
           Vi er en gruppe af unge, som har været med i udviklingen af
           kommunikationsteknikker i de sidste år.
         </h2>
-        <Link href="/#kontakt">
+        <Link href="/kontakt">
           <div className="cursor-pointer rounded-md px-4 py-2 lg:px-10 lg:text-2xl lg:py-4 uppercase bg-gradient-to-r from-pink-500 to-violet-500 font-black text-white">
             Kontakt os
           </div>
